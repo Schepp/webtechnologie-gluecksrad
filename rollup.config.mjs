c6324b78fbd782545edf1b65436675fbc75aae68
Christian Schaefer <schaepp@gmx.de>
2024-01-21 13:05:51 +0100

    Updates dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,10 @@
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import css from 'rollup-plugin-css-only';
+import terser from '@rollup/plugin-terser';
 import json from '@rollup/plugin-json';
+import livereload from 'rollup-plugin-livereload';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
